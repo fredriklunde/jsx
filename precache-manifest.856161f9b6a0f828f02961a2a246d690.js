87280562ffe28ae826acc7c97acecec1434c3bd0
fredriklunde <fredrik.lunde3@gmail.com>
2019-01-20 11:13:24 +0100

    Updates

---
+++ precache-manifest.856161f9b6a0f828f02961a2a246d690.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf66ce06f9514a75d5c9",
+    "url": "/jsx/static/js/main.cf66ce06.chunk.js"
+  },
+  {
+    "revision": "c2ea6c283125f00601cc",
+    "url": "/jsx/static/js/1.c2ea6c28.chunk.js"
+  },
+  {
+    "revision": "31bcc97bda4e250142fe",
+    "url": "/jsx/static/js/runtime~main.31bcc97b.js"
+  },
+  {
+    "revision": "166297286bbe59189ec3d6535cbecdcd",
+    "url": "/jsx/index.html"
+  }
+];
---
+++ static/js/main.cf66ce06.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],[,,,function(e,t,n){e.exports=n(4)},function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),r=n(2);n.n(r).a.render(l.a.createElement(function(){return l.a.createElement("div",null,l.a.createElement("label",{className:"label",htmlFor:"name"},"Enter name:"),l.a.createElement("input",{id:"name",type:"text"}),l.a.createElement("button",{style:{backgroundColor:"blue",color:"white"}},"click me"))},null),document.querySelector("#root"))}],[[3,2,1]]]);
+//# sourceMappingURL=main.cf66ce06.chunk.js.map
